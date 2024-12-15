138c2c4cbb63b684083c048db5e8c8c15e9fa94f
Jonathan Sanfilippo <jonjon5997@gmail.com>
2024-12-15 17:06:33 -0500

    styles and weather card

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,7 +1,10 @@
+import React, { useContext } from "react";
 import "./WeatherCard.css";
 import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
+import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
 
 function WeatherCard({ weatherData }) {
+  const { currentTempUnit } = useContext(CurrentTemperatureUnitContext);
   const filteredOptions = weatherOptions.filter((option) => {
     // filter method - returns an array d=subject to the conditions youve left in it
     return (
@@ -16,9 +19,18 @@ function WeatherCard({ weatherData }) {
     weatherOption = filteredOptions[0];
   }
 
+  // Determine the temperature to display
+  const displayTemp = weatherData.temp[currentTempUnit];
+
+  console.log("Current Temperature Unit:", currentTempUnit);
+  console.log("Weather Data Temp:", weatherData.temp);
+  console.log("Display Temp:", weatherData.temp[currentTempUnit]);
+
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
+      <p className="weather-card__temp">
+        {displayTemp} &deg; {currentTempUnit}
+      </p>
       <img
         src={weatherOption?.url}
         alt={`Card showing ${weatherOption?.day ? "day" : "night"} ${
