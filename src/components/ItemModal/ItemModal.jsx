a3b7f5e4de7d4e674629a1c466e1293f28d769e5
Jonathan Sanfilippo <jonjon5997@gmail.com>
2024-12-23 12:56:04 -0500

    styles changes

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,78 +1,3 @@
-// import "./ItemModal.css";
-// import { defaultClothingItems } from "../../utils/constants";
-
-// function ItemModal({ activeModal, handleCloseClick, card }) {
-//   if (!card) {
-//     return null;
-//   }
-
-//   return (
-//     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
-//       <div className="modal__content modal__content_type_image">
-//         <button
-//           onClick={handleCloseClick}
-//           type="button"
-//           className="modal__close"
-//         ></button>
-//         <img
-//           src={card.link || "path-to-default-image.jpg"}
-//           alt={card.name || "Item Image"}
-//           className="modal__image"
-//         />
-//         <div className="modal_footer">
-//           <h2 className="modal__caption">{card.name}</h2>
-//           <p className="modal__weather">Weather: {card.weather}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ItemModal;
-
-// import "./ItemModal.css";
-
-// function ItemModal({ activeModal, handleCloseClick, card }) {
-//   if (!card) {
-//     return null; // Prevent rendering if no card is passed
-//   }
-
-//   const handleDeleteClick = () => {
-//     onDeleteCard(card.id); // Call the delete handler with the card's ID
-//   };
-
-//   return (
-//     <div className={`modal ${activeModal === "preview" ? "modal_opened" : ""}`}>
-//       <div className="modal__content modal__content_type_image">
-//         <button
-//           onClick={handleCloseClick}
-//           type="button"
-//           className="modal__close"
-//           aria-label="Close modal"
-//         ></button>
-//         <img
-//           src={card.link || "path-to-default-image.jpg"} // Use fallback if card.link is not provided
-//           alt={card.name || "Item Image"}
-//           className="modal__image"
-//         />
-//         <div className="modal_footer">
-//           <h2 className="modal__caption">{card.name || "No name provided"}</h2>
-//           <p className="modal__weather">
-//             Weather: {card.weather || "No weather info"}
-//           </p>
-
-//           {/* Delete Button */}
-//           <button className="modal__delete-button" onClick={handleDeleteClick}>
-//             Delete Card
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ItemModal;
-
 import React from "react";
 import "./ItemModal.css";
 
@@ -95,7 +20,7 @@ const ItemModal = ({ activeModal, handleCloseClick, card, onDeleteCard }) => {
           className="modal__close"
         ></button>
         <img
-          src={card.link}
+          src={card.imageUrl}
           alt={card.name || "Item Image"}
           className="modal__image"
         />
@@ -106,7 +31,7 @@ const ItemModal = ({ activeModal, handleCloseClick, card, onDeleteCard }) => {
 
         {/* Delete Button */}
         <button className="modal__delete-button" onClick={handleDeleteClick}>
-          Delete Card
+          Delete Item
         </button>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </StrictMode>
 );
