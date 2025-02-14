e0c1d61767e57dc05f08d3a7bf478f082db0a144
Jonathan Sanfilippo <jonjon5997@gmail.com>
2025-02-14 12:02:26 -0500

    update header include profile link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,115 @@
+// import logo from "../../assets/logo.svg";
+// import avatarPlaceholder from "../../assets/avatar.svg"; // Fallback avatar
+// import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+// import "./Header.css";
+// import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperatureUnitContext";
+// import { useContext, useState } from "react";
+// import { Link } from "react-router-dom";
+// import CurrentUserContext from "../../contexts/CurrentUserContext";
+
+// function Header({
+//   handleAddClick,
+//   temp,
+//   weatherData,
+//   openLoginModal,
+//   openRegisterModal,
+//   handleSignOut,
+// }) {
+//   const { currentTempUnit, handleToggleSwitchChange } = useContext(
+//     CurrentTemperatureUnitContext
+//   );
+//   const { currentUser, isLoggedIn } = useContext(CurrentUserContext);
+
+//   if (!weatherData || !temp) {
+//     return null; // Or render a loading indicator
+//   }
+//   const currentDate = new Date().toLocaleString("default", {
+//     month: "long",
+//     day: "numeric",
+//   });
+
+//   // Generate a placeholder avatar (first letter of the user's name)
+//   const getInitials = (name) => (name ? name.charAt(0).toUpperCase() : "?");
+
+//   return (
+//     <header className="header">
+//       <Link to="/">
+//         <img className="header__logo" src={logo} alt="App Logo" />
+//       </Link>
+//       <p className="header__date-and-location">
+//         {currentDate}, {weatherData.city} {temp[currentTempUnit]}°{" "}
+//         {currentTempUnit}
+//       </p>
+//       <div className="header__profile-container">
+//         <ToggleSwitch />
+
+//         {isLoggedIn ? (
+//           <>
+//             <button
+//               onClick={handleAddClick}
+//               type="button"
+//               className="header__add-clothes-btn"
+//             >
+//               + Add Clothes
+//             </button>
+//             <div className="header__user-container">
+//               <p className="header__username">{currentUser?.name}</p>
+//               {currentUser?.avatar ? (
+//                 <img
+//                   src={currentUser.avatar}
+//                   alt={currentUser.name}
+//                   className="header__avatar"
+//                 />
+//               ) : (
+//                 <div className="header__avatar header__avatar--placeholder">
+//                   {getInitials(currentUser?.name)}
+//                 </div>
+//               )}
+//               <button
+//                 className="header__signout-button"
+//                 onClick={handleSignOut}
+//               >
+//                 Sign Out
+//               </button>
+//             </div>
+//           </>
+//         ) : (
+//           <div className="header__auth">
+//             <button
+//               onClick={openLoginModal}
+//               className="header__link header__link-login"
+//             >
+//               Log In
+//             </button>
+//             <button
+//               onClick={openRegisterModal}
+//               className="header__link header__link-signup"
+//             >
+//               Sign Up
+//             </button>
+//           </div>
+//         )}
+//       </div>
+//       {/* {isLoginModalOpen && (
+//         <LoginModal
+//           closeModal={closeModals}
+//           handleLogin={setLoginModalOpen}
+//           isOpen={isLoginModalOpen}
+//         />
+//       )}
+//       {isRegisterModalOpen && (
+//         <RegisterModal
+//           closeModal={closeModals}
+//           handleRegistration={setRegisterModalOpen}
+//           isOpen={isRegisterModalOpen}
+//         />
+//       )} */}
+//     </header>
+//   );
+// }
+
+// export default Header;
+
 import logo from "../../assets/logo.svg";
 import avatarPlaceholder from "../../assets/avatar.svg"; // Fallback avatar
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
@@ -6,8 +118,6 @@ import { CurrentTemperatureUnitContext } from "../../contexts/CurrentTemperature
 import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
-import LoginModal from "../LoginModal/LoginModal";
-import RegisterModal from "../RegisterModal/RegisterModal";
 
 function Header({
   handleAddClick,
@@ -55,7 +165,9 @@ function Header({
               + Add Clothes
             </button>
             <div className="header__user-container">
-              <p className="header__username">{currentUser?.name}</p>
+              <Link to="/profile" className="header__link header__link-profile">
+                <p className="header__username">{currentUser?.name}</p>
+              </Link>
               {currentUser?.avatar ? (
                 <img
                   src={currentUser.avatar}
@@ -67,6 +179,8 @@ function Header({
                   {getInitials(currentUser?.name)}
                 </div>
               )}
+              {/* Link to the profile page */}
+
               <button
                 className="header__signout-button"
                 onClick={handleSignOut}
@@ -92,20 +206,6 @@ function Header({
           </div>
         )}
       </div>
-      {/* {isLoginModalOpen && (
-        <LoginModal
-          closeModal={closeModals}
-          handleLogin={setLoginModalOpen}
-          isOpen={isLoginModalOpen}
-        />
-      )}
-      {isRegisterModalOpen && (
-        <RegisterModal
-          closeModal={closeModals}
-          handleRegistration={setRegisterModalOpen}
-          isOpen={isRegisterModalOpen}
-        />
-      )} */}
     </header>
   );
 }
